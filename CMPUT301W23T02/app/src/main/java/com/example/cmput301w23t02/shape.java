0a70e1fff48ca4eb480102acf376488ba35cddbe
Elisandro Cruz Martinez <cruzmart@ualberta.ca>
2023-01-31 18:37:44 -0700

    changes had been made

--- CMPUT301W23T02/app/src/main/java/com/example/cmput301w23t02/shape.java
+++ CMPUT301W23T02/app/src/main/java/com/example/cmput301w23t02/shape.java
@@ -1,7 +1,10 @@
 package com.example.cmput301w23t02;
 
+import android.graphics.drawable.shapes.Shape;
+
 public abstract class shape {
     private int x = 0, y = 0;
+    String color = "blue";
     public shape(int x, int y)
     {
         this.x = x;
