d32cb2cd629a4298901f50c9511891a9a4160a3a
Ryan Rom <rom@ualberta.ca>
2023-01-31 19:54:39 -0700

    I think color = blue should not be on the main branch

--- CMPUT301W23T02/app/src/main/java/com/example/cmput301w23t02/shape.java
+++ CMPUT301W23T02/app/src/main/java/com/example/cmput301w23t02/shape.java
@@ -3,7 +3,6 @@ package com.example.cmput301w23t02;
 import android.graphics.drawable.shapes.Shape;
 
 public abstract class shape {
-    String color = "blue";
     private int x = 0, y = 0;
     public shape(int x, int y)
     {
