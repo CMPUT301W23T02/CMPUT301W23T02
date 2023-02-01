23442da0a15c329d9e4a4baffb147cabea19d35f
Elisandro Cruz Martinez <cruzmart@ualberta.ca>
2023-02-01 09:48:54 -0700

    method 'circle_radius' has been added to Circle class

--- CMPUT301W23T02/app/src/main/java/com/example/cmput301w23t02/Circle.java
+++ CMPUT301W23T02/app/src/main/java/com/example/cmput301w23t02/Circle.java
@@ -1,8 +1,12 @@
 package com.example.cmput301w23t02;
+import static java.lang.Math.*;
 
 public class Circle extends shape {
     private Integer r;
     public Circle(int x, int y) {
         super(x, y);
+    }
+    public float circle_radius(){
+        return (float) (PI * (r^2));
     }
 }
