da4f7bdc08968050c84bfc87d8626aa16bc7fc77
Shen Ruijun <ruijun2@ualberta.ca>
2023-01-31 17:57:05 -0700

    Ruijun Shen's edit

---
+++ CMPUT301W23T02/app/src/main/java/com/example/cmput301w23t02/shape.java
@@ -0,0 +1,10 @@
+package com.example.cmput301w23t02;
+
+public abstract class shape {
+    private int x = 0, y = 0;
+    public shape(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
