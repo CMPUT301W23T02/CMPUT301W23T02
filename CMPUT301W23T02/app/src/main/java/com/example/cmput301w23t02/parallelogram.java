0fca923b276e2a9461e181a9b9c8bf885fb987dc
ssehrawat01 <ssehrawa@ualberta.ca>
2023-02-02 17:50:20 -0700

    new shape

---
+++ CMPUT301W23T02/app/src/main/java/com/example/cmput301w23t02/parallelogram.java
@@ -0,0 +1,10 @@
+package com.example.cmput301w23t02;
+
+public class parallelogram extends shape {
+    public parallelogram(int x, int y){
+        super(x, y);
+    }
+    public int area(){
+        return x * y ;
+    }
+}
