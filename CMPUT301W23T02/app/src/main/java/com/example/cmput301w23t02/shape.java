aa9a7c7a2c1d7704ab3cdcbdf4736a036033bece
Shubhangi Sehrawat <90876253+ssehrawat01@users.noreply.github.com>
2023-02-02 18:34:15 -0700

    Update shape.java

--- CMPUT301W23T02/app/src/main/java/com/example/cmput301w23t02/shape.java
+++ CMPUT301W23T02/app/src/main/java/com/example/cmput301w23t02/shape.java
@@ -3,7 +3,7 @@ package com.example.cmput301w23t02;
 import android.graphics.drawable.shapes.Shape;
 
 public abstract class shape {
-    String color = "red";
+    String color = "blue";
     protected int x = 0, y = 0;
     public shape(int x, int y)
     {
